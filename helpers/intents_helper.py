5a81da178f93982aaa82437a63b45737c4f561ac
ivanbozhko <Ivan_Bozhko@epam.com>
2024-03-15 13:24:48 +0200

    Updated intents data file

--- helpers/intents_helper.py
+++ helpers/intents_helper.py
@@ -6,7 +6,7 @@ def load_intents_file():
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path = os.path.join(
         parent_dir,
-        "intents_2024-03-14T22-57-23.json"  # name of the intents data file should be passed here
+        "intents_2024-03-15T13-19-37.json"  # name of the intents data file should be passed here
     )
     with open(path) as file:
         data = json.load(file)
